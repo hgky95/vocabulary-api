6cee8c644ca026d8357370f458a1a7fc38fdba31
Ky Huynh Gia <hgky@axonactive.com>
2020-11-11 12:34:03 +0700

    draft implementation

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <input v-model="words" placeholder="Input words">
+  <button v-on:click="getEntry">Search</button>
+  <ul v-if="entries && entries.length">
+
+    <li v-for="entry in entries" :key="entry.word">
+      <p><strong>{{entry.word}}</strong></p>
+      <p>{{entry.phonetics}}</p>
+      <p>{{entry.meanings}}</p>
+    </li>
+  </ul>
+
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      entries: [],
+      words: []
+    }
+  },
+
+  methods: {
+    getEntry : function () {
+      console.log(this.words);
+      let url = `https://api.dictionaryapi.dev/api/v2/entries/en/` + this.words;
+    axios.get(url)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.entries = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+
+  // Fetches posts when the component is created.
+  
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
