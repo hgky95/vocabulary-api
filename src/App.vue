5d4e3edac99ab33d28a6a5b4336ad458b987f007
Ky Huynh <ky.huynh@axonactive.com>
2020-12-09 13:01:50 +0700

    add primeflex and build template

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,45 @@
 <style>
   @import './assets/styles/custom.css';
+
 </style>
 
 <template>
-  <input v-model="words" placeholder="Input words" v-on:keyup.enter="getEntry">
-  <button v-on:click="getEntry">Search</button>
-  <ul v-if="entries && entries.length">
+  <div class="p-grid"> 
+    <input class="p-col-2" v-model="words" placeholder="Input words" v-on:keyup.enter="getEntry">
+    <button v-on:click="getEntry">Search</button>
+  </div> 
+      <ul v-if="entries && entries.length">
+        <li v-for="entry in entries" :key="entry.word" class="p-col-4">
+          <p><strong>{{entry.word}}</strong></p>
+          <div v-for="phonetic in entry.phonetics" :key="phonetic.text">
+            <p>Phonetic: {{phonetic.text}}</p>
+            <button v-on:click="downloadMp3(phonetic.audio)">DownLoad Audio</button>
+          </div>
 
-    <li v-for="entry in entries" :key="entry.word">
-      <p><strong>{{entry.word}}</strong></p>
-      <div v-for="phonetic in entry.phonetics" :key="phonetic.text">
-        <p>Phonetic: {{phonetic.text}}</p>
-        <button v-on:click="downloadMp3(phonetic.audio)">DownLoad Audio</button>
-      </div>
-
-      <div v-for="meaning in entry.meanings" :key="meaning.partOfSpeech">
-        <ul>
-          <li>
-            <p>Part of Speech: {{meaning.partOfSpeech}}</p>
-            <ul v-for="definition in meaning.definitions" :key="definition.definition">
-              <span>
-                <li> <p>Definition: {{definition.definition}}</p> </li>
-                <li v-if="definition.example"> <p>Example: {{definition.example}} </p> </li>
-                <li v-if="definition.synonyms"> <p>Synonyms: {{definition.synonyms}} </p> </li>
-                <hr>
-              </span>
+          <div v-for="meaning in entry.meanings" :key="meaning.partOfSpeech">
+            <div class="p-d-flex p-flex-column"> 
+              <ul>
+                <div class="p-mr-2">
+                  <li>
+                    <p>Part of Speech: {{meaning.partOfSpeech}}</p>
+                    <ul v-for="definition in meaning.definitions" :key="definition.definition">
+                      <span>
+                        <li> <p>Definition: {{definition.definition}}</p> </li>
+                        <li v-if="definition.example"> <p>Example: {{definition.example}} </p> </li>
+                        <li v-if="definition.synonyms"> <p>Synonyms: {{definition.synonyms}} </p> </li>
+                        <hr>
+                      </span>
+                    </ul>
+                  </li>
+                </div>
               </ul>
-          </li>
-        </ul>
-      </div>
-    </li>
-  </ul>
+            </div>
+            
+          </div>
+        </li>
+      </ul>
+
+
 
 
   
