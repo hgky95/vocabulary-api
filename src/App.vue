530ffb8e06b39871e9176aab9896cf4d390c8288
hgky95 <hgky95@gmail.com>
2020-12-24 17:51:20 +0700

    custom template

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,28 @@
 </style>
 
 <template>
-  <div class="p-grid"> 
-    <input class="p-col-2" v-model="words" placeholder="Input words" v-on:keyup.enter="getEntry">
-    <button v-on:click="getEntry">Search</button>
-  </div> 
+  <section>
+      
+        <h2> Dictionary </h2>
+        <input class="p-col-2" v-model="words" placeholder="Input words" v-on:keyup.enter="getEntry">
+        <button v-on:click="getEntry">Search</button>
+     
       <ul v-if="entries && entries.length">
-        <li v-for="entry in entries" :key="entry.word" class="p-col-4">
-          <p><strong>{{entry.word}}</strong></p>
+        <li v-for="entry in entries" :key="entry.word">
+          <p class="p-col-4">{{entry.word}}</p>
           <div v-for="phonetic in entry.phonetics" :key="phonetic.text">
-            <p>Phonetic: {{phonetic.text}}</p>
+            <p class="p-col-4">Phonetic: {{phonetic.text}}</p>
             <button v-on:click="downloadMp3(phonetic.audio)">DownLoad Audio</button>
           </div>
 
           <div v-for="meaning in entry.meanings" :key="meaning.partOfSpeech">
-            <div class="p-d-flex p-flex-column"> 
+            <div class=""> 
               <ul>
-                <div class="p-mr-2">
+                <div>
                   <li>
                     <p>Part of Speech: {{meaning.partOfSpeech}}</p>
                     <ul v-for="definition in meaning.definitions" :key="definition.definition">
-                      <span>
+                      <span class="shadow">
                         <li> <p>Definition: {{definition.definition}}</p> </li>
                         <li v-if="definition.example"> <p>Example: {{definition.example}} </p> </li>
                         <li v-if="definition.synonyms"> <p>Synonyms: {{definition.synonyms}} </p> </li>
@@ -38,6 +40,8 @@
           </div>
         </li>
       </ul>
+  </section>
+  
 
 
 
