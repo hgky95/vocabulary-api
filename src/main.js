91653acc3638a42051be0402e8e217f7e389fec6
hgky95 <hgky95@gmail.com>
2020-12-29 12:30:49 +0700

    apply PrimeVue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'primeflex/primeflex.css' 
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.mount('#app')
