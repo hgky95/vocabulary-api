a101361183fe6c573e787c6119a1ac907f3a5b81
Ky Huynh <hgky95@gmail.com>
2021-02-12 00:38:59 +0700

    enable CORS and create a new method to download the audio

--- backend/src/main/java/com/english/dictionary/controller/DictionaryController.java
+++ backend/src/main/java/com/english/dictionary/controller/DictionaryController.java
@@ -1,6 +1,7 @@
 package com.english.dictionary.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class DictionaryController {
 
 	private static final String URI = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 	
+	@CrossOrigin
 	@GetMapping("/dictionary/{word}")
     public String getWord(@PathVariable String word) {
 		RestTemplate restTemplate = new RestTemplate();
@@ -18,4 +20,14 @@ public class DictionaryController {
 		ResponseEntity<String> result = restTemplate.getForEntity(destinationURI, String.class);
         return result.getBody();
     }
+	
+	@CrossOrigin
+	@GetMapping("/audio/{url}")
+	public String downloadAudio(@PathVariable String audioUrl) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> result = restTemplate.getForEntity(audioUrl, String.class);
+        return result.getBody();
+	}
+	
+	
 }
